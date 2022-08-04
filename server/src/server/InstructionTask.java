9a4fe7eed514d1a4b5d208a0cb4dfc8fe83ebbd7
xiaohei7012 <xiaohei7012@163.com>
2022-08-04 17:34:50 +0800

    添加代码

--- server/src/server/InstructionTask.java
+++ server/src/server/InstructionTask.java
@@ -2,8 +2,13 @@ package server;
 
 import java.util.TimerTask;
 
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
+
 public class InstructionTask extends TimerTask{
 	private SimpleServer server;
+	private Scheduler scheduler;
 	
 	public InstructionTask(SimpleServer server) {
 		this.server = server;
@@ -11,6 +16,18 @@ public class InstructionTask extends TimerTask{
 
 	@Override
 	public void run() {
-		
+		try {
+			//每次运行调度器清理上次的任务 然后重新根据方案生成调度
+			//每个方案生成一个调度 每个调度查询有多少个设备 每个设备发送命令
+			if(scheduler!=null&&scheduler.isStarted()) {
+				scheduler.pauseAll();
+				scheduler.shutdown();
+			}
+			scheduler = StdSchedulerFactory.getDefaultScheduler();
+			
+			
+		} catch (SchedulerException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- server/src/server/LightServer.java
+++ server/src/server/LightServer.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.nio.channels.SocketChannel;
+import java.util.Timer;
 
 public class LightServer extends SimpleServer {
 	
@@ -13,6 +14,12 @@ public class LightServer extends SimpleServer {
 		server.start();
 	}
 
+	public void start() {
+		super.start();
+//		new Timer().schedule(new InstructionTask(this), 24* 60* 60 * 1000);
+		new Timer().schedule(new InstructionTask(this), 5 * 1000);
+	}
+	
 	@Override
 	public void onConnected(SocketChannel sc) {
 		socketMap.put(sc.socket().getInetAddress().getHostAddress(),sc);
